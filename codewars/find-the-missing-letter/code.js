7afb5c446d53eccda42949c62c0db6a0d6d939c1
kkapchin <kkapchin@yandex.ru>
2022-10-16 17:48:35 +0800

    find-the-missing-letter2

--- codewars/find-the-missing-letter/code.js
+++ codewars/find-the-missing-letter/code.js
@@ -1,5 +1,5 @@
 function findMissingLetter(array) {
-    const alphabet = ['a','b','c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A','B','C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     const row = alphabet.slice(alphabet.indexOf(array[0]), alphabet.indexOf(array[array.length - 1]) + 1);
     console.log(array)
     console.log(row)
