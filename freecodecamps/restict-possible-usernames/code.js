f807d4e789c8bcd9c7b3ba2ece39ee15ae8952fb
kkapchin <kkapchin@yandex.ru>
2022-10-18 01:06:44 +0800

    restrict-possible-usernames

---
+++ freecodecamps/restict-possible-usernames/code.js
@@ -0,0 +1,8 @@
+let username = "JackOfAllTrades";
+//have to begin with the letters => ^[a-z]
+//zero or more digits ath the end => \d for digits, * for zero or more, $ for the end
+// i for ignore uppercase or lowercase
+//^[a-z][a-z]+ => at least two characters long and two letters long usernames must be alphabetic
+// | stands for 'or'
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Change this line
+let result = userCheck.test(username);
